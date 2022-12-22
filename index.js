d40cb45de6f8cf736d526ebc4d757c00838ad0be
Dika Handika <dika.handika@paper.id>
2022-12-22 10:05:51 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const { google } = require('googleapis');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 8080;
+
+app.use(cors());
+
+app.post('/get-sheet-by-sheet-name', async (req, res) => {
+  const { spreadsheetId, sheetName } = req.body;
+
+  const auth = new google.auth.GoogleAuth({
+    keyFile: 'creds.json',
+    scopes: 'https://www.googleapis.com/auth/spreadsheets',
+  });
+  const client = await auth.getClient();
+  const googleSheets = google.sheets({ version: 'v4', auth: client });
+
+  let response;
+
+  try {
+    response = await googleSheets.spreadsheets.values.get({
+      auth,
+      spreadsheetId,
+      range: sheetName,
+      valueRenderOption: 'UNFORMATTED_VALUE',
+    });
+
+    const { data, status, statusText } = response;
+
+    res.send({ data, status, statusText });
+  } catch ({ code, errors }) {
+    res.status(code).send(errors);
+  }
+});
+
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Example app listening on port ${port}`);
+});
