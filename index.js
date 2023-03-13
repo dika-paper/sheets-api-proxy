e1151173747a3a99a91438cd519b93d11e7b5a5c
Dika Handika <dika.handika@paper.id>
2023-03-13 11:45:16 +0700

    feat: update sheet value
    endpoint for updating sheet value

--- index.js
+++ index.js
@@ -39,6 +39,36 @@ app.post('/get-sheet-by-sheet-name', async (req, res) => {
   }
 });
 
+app.post('/update', async (req, res) => {
+  const {
+    spreadsheetId, range, valueInputOption, resource,
+  } = req.body;
+
+  const auth = new google.auth.GoogleAuth({
+    keyFile: 'creds.json',
+    scopes: 'https://www.googleapis.com/auth/spreadsheets',
+  });
+  const client = await auth.getClient();
+  const googleSheets = google.sheets({ version: 'v4', auth: client });
+
+  let response;
+
+  try {
+    response = await googleSheets.spreadsheets.values.update({
+      spreadsheetId,
+      range,
+      valueInputOption,
+      resource,
+    });
+
+    const { data, status, statusText } = response;
+
+    res.send({ data, status, statusText });
+  } catch ({ code, errors }) {
+    res.status(code).send(errors);
+  }
+});
+
 app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(`Example app listening on port ${port}`);
